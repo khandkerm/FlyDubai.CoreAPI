12f1c2f6f9b5df78bdb22582c54de623656ed7bb
Md. Abir Hossain <abir.user@gmail.com>
2024-06-10 14:39:57 +0600

    cp service added

--- FlyDubai.CoreAPI.Services/Contracts/ICP.cs
+++ FlyDubai.CoreAPI.Services/Contracts/ICP.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using FlyDubai.CoreAPI.Models.Requests;
+﻿using FlyDubai.CoreAPI.Models.Requests;
 using FlyDubai.CoreAPI.Models.Responses;
 
 namespace FlyDubai.CoreAPI.Services.Contracts
 {
     public interface ICP
     {
-        Task<SummaryPNRResponse> SummaryPNRAsync(SummaryPNRRequest summaryPNRRequest);
+        Task<SummaryPNRResponse> SummaryPNRAsync(SummaryPNRRequest request, string endpointBaseUrl, string accessToken);
     }
 }
--- FlyDubai.CoreAPI/Controllers/CPController.cs
+++ FlyDubai.CoreAPI/Controllers/CPController.cs
@@ -13,22 +13,23 @@ namespace FlyDubai.CoreAPI.Controllers
     /// <summary>
     /// 
     /// </summary>
-    /// <param name="service"></param>
     /// <param name="logger"></param>
+    /// <param name="service"></param>
+    /// <param name="flyService"></param>
     /// <param name="cache"></param>
     /// <param name="appSettings"></param>
-
     [Authorize]
     [ApiController]
     [ApiVersion("1.0")]
     [ValidateAntiForgeryToken]
     [Route("api/cp")]
     [Route("api/v{version:apiVersion}/cp")]
-    public class CPController(ICP service, ILogger<CPController> logger, FlyDubaiCache cache, IOptions<AppSettings> appSettings) : ControllerBase
+    public class CPController(ILogger<CPController> logger, ICP service, IFlyDubai flyService, IFlyDubaiCache cache, IOptions<AppSettings> appSettings) : ControllerBase
     {
-        private readonly ILogger _logger = logger;
         private readonly ICP _service = service;
+        private readonly ILogger _logger = logger;
         private readonly IFlyDubaiCache _cache = cache;
+        private readonly IFlyDubai _flyService = flyService;
 
         private readonly AppSettings _appSettings = appSettings.Value;
         private readonly DateTimeOffset _options = Helper.Helper.CreateCollectoCacheOptions();
@@ -36,17 +37,51 @@ namespace FlyDubai.CoreAPI.Controllers
         /// <summary>
         /// 
         /// </summary>
-        /// <param name="summaryPNRRequest"></param>
+        /// <param name="request"></param>
         /// <returns></returns>
         [HttpPost("summaryPNR")]
-        public async Task<IActionResult> SummaryPNR([FromBody] SummaryPNRRequest summaryPNRRequest)
+        [AllowAnonymous]
+        [IgnoreAntiforgeryToken]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(SummaryPNRResponse))]
+        public async Task<IActionResult> SummaryPNR([FromBody] SummaryPNRRequest request)
         {
-
             SummaryPNRResponse response = new();
+
+            if (request == null)
+            {
+                response.ReturnStatus = StatusCodes.Status417ExpectationFailed;
+                response.ReturnMessage.Add("Parameter value is null.");
+                return StatusCode(StatusCodes.Status417ExpectationFailed, response);
+            }
+
+            if (string.IsNullOrEmpty(_appSettings.EndpointBaseUrl))
+            {
+                response.ReturnStatus = StatusCodes.Status417ExpectationFailed;
+                response.ReturnMessage.Add("Endpoint Base Url value is null.");
+                return StatusCode(StatusCodes.Status417ExpectationFailed, response);
+            }
             
             try
             {
-                response = await _service.SummaryPNRAsync(summaryPNRRequest);
+                LoginRequest loginRequest = new()
+                {
+                    ClientId = _appSettings.ClientId,
+                    ClientSecret = _appSettings.ClientSecret,
+                    Username = _appSettings.Username,
+                    Password = _appSettings.Password
+                };
+
+                AccessTokenResponse accessTokenResponse = new();
+
+                string key = $"Authenticate~{loginRequest.ClientId}~{loginRequest.ClientSecret}~{loginRequest.Username}~{loginRequest.Password}";
+                if (_cache.TryGetValue(key: key, value: out accessTokenResponse) == false)
+                {
+                    accessTokenResponse = await _flyService.AuthenticateAsync(loginRequest);
+                    //Cache
+                    _ = _cache.Set(key: key, value: accessTokenResponse, options: _options);
+                }
+
+                response = await _service.SummaryPNRAsync(request: request, endpointBaseUrl: _appSettings.EndpointBaseUrl, accessToken: accessTokenResponse.AccessToken); ;
             }
             catch (System.Exception ex)
             {
